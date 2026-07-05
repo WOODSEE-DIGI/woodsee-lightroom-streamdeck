2d4449e8cb95724dcd402c8fa346e57b280c038e
WOODSEE-DIGI <118095876+WOODSEE-DIGI@users.noreply.github.com>
2026-07-05 20:58:24 +1000

    Remove duplicate installer .app from git, remove dead bridge.ts

--- installer/Woodsee's Lightroom Stream Deck Installer.app/Contents/Resources/com.woodseedigi.lightroom.mystory.sdPlugin/bin/plugin.js
+++
@@ -1,7 +0,0 @@
-import streamDeck from "@elgato/streamdeck";
-import { tagActions } from "./actions/tagAction.js";
-for (const act of tagActions) {
-    streamDeck.actions.registerAction(act);
-}
-streamDeck.connect();
-//# sourceMappingURL=plugin.js.map
